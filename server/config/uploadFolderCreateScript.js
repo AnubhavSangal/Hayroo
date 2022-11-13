1a3520e60d00f94192b6e4b6f37904072b8ce6fd
AnubhavSangal <anubhavsangal12345@gmail.com>
2022-11-14 01:38:27 +0530

    changes

--- server/config/uploadFolderCreateScript.js
+++ server/config/uploadFolderCreateScript.js
@@ -5,23 +5,23 @@ const customizeFolder = "./public/uploads/customize";
 const productsFolder = "./public/uploads/products";
 
 const CreateAllFolder = () => {
-  // if (!fs.existsSync(categoriesFolder)) {
-  //   fs.mkdirSync(categoriesFolder, {
-  //     recursive: true,
-  //   });
-  // }
+  if (!fs.existsSync(categoriesFolder)) {
+    fs.mkdirSync(categoriesFolder, {
+      recursive: true}, err => {}
+    );
+  }
 
-  // if (!fs.existsSync(customizeFolder)) {
-  //   fs.mkdirSync(customizeFolder, {
-  //     recursive: true,
-  //   });
-  // }
+  if (!fs.existsSync(customizeFolder)) {
+    fs.mkdirSync(customizeFolder, {
+      recursive: true,
+    });
+  }
 
-  // if (!fs.existsSync(productsFolder)) {
-  //   fs.mkdirSync(productsFolder, {
-  //     recursive: true,
-  //   });
-  // }
+  if (!fs.existsSync(productsFolder)) {
+    fs.mkdirSync(productsFolder, {
+      recursive: true,
+    });
+  }
 };
 
 module.exports = CreateAllFolder;
