5011d61cb18b92eb36a485b9b05e4e87ac57c393
AnubhavSangal <anubhavsangal12345@gmail.com>
2022-11-14 11:21:48 +0530

    changes

--- server/config/uploadFolderCreateScript.js
+++ server/config/uploadFolderCreateScript.js
@@ -1,8 +1,8 @@
 const fs = require("fs");
 
-const categoriesFolder = "/var/task/server/public/uploads/categories";
-const customizeFolder = "/var/task/server/public/uploads/customize";
-const productsFolder = "/var/task/server/public/uploads/products";
+const categoriesFolder = "./public/uploads/categories";
+const customizeFolder = "./public/uploads/customize";
+const productsFolder = "./public/uploads/products";
 
 const CreateAllFolder = () => {
   if (!fs.existsSync(categoriesFolder)) {
